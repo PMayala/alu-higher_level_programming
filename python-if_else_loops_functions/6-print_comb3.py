c8c484302d2fc0c68b9d822a1ccdc01bf4dba96c
PMayala <p.mayala@alustudent.com>
2023-11-01 23:05:08 -0700

    COmmit 2

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(i, 10):
+        if i == j:
+            continue
+        if i == 8 and j == 9:
+            continue
+        print("{0}{1}".format(i, j), end=", ")
+print("{}".format(89))
