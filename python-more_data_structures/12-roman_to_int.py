2fe458d294616631585405a1ef813d4e0d7e2d52
PMayala <p.mayala@alustudent.com>
2023-11-04 13:04:53 -0700

    Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,41 @@
-#!/usr/bin/python
 def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+    
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    prev_value = 0
+    
+    for char in roman_string[::-1]:
+        if roman_dict[char] >= prev_value:
+            total += roman_dict[char]
+        else:
+            total -= roman_dict[char]
+        prev_value = roman_dict[char]
+    
+    return total
+
+# Test the function
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
