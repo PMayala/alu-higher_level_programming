58dd1162c47e6081742951392c9e416a229a052c
PMayala <p.mayala@alustudent.com>
2023-11-04 12:29:45 -0700

    7-add_tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        x, y = (0, 1)
-        tuple_sumx = tuple_a[x] + tuple_b[x]
-        tuple_sumy = tuple_a[y] + tuple_b[y]
-        return (tuple_sumx, tuple_sumy)
+    # Get the first two elements of each tuple or use 0 if the tuple is smaller
+    x1, y1 = tuple_a[:2] if len(tuple_a) >= 2 else (0, 0)
+    x2, y2 = tuple_b[:2] if len(tuple_b) >= 2 else (0, 0)
+
+    # Calculate the sum of the corresponding elements
+    tuple_sum = (x1 + x2, y1 + y2)
+    
+    return tuple_sum
