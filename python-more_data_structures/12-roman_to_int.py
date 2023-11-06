b42985217b49ec2682ab1bb9830638310e79f9fc
PMayala <p.mayala@alustudent.com>
2023-11-06 00:28:24 -0800

    update dictionary

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,25 +1,13 @@
+#!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
+    if not roman_string or type(roman_string) != str:
         return 0
-    
-    roman_dict = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    
-    total = 0
-    prev_value = 0
-    
-    for char in roman_string[::-1]:
-        if roman_dict[char] >= prev_value:
-            total += roman_dict[char]
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_n = 0
+    for j in range(len(roman_string)):
+        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
+            roman_n += roman_d[roman_string[j]] - 2 * \
+                        roman_d[roman_string[j - 1]]
         else:
-            total -= roman_dict[char]
-        prev_value = roman_dict[char]
-    
-    return total
+            roman_n += roman_d[roman_string[j]]
+    return roman_n
