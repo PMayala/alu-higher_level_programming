5b68dc73a3e6726a7954b82a2c08f8b99b18c9bf
PMayala <p.mayala@alustudent.com>
2023-11-13 02:26:38 -0800

    10-square

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Defines class Square"""
-
-Rectangle = __import__("9-rectangle").Rectangle
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ Square that inherits from Rectangle """
+    """Represent a square."""
 
     def __init__(self, size):
-        """ Instantiation with width """
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.integer_validator("size", size)
-        try:
-            super().__init__(size, size)
-        except TypeError:
-            raise TypeError("size must be an integer")
-        except ValueError:
-            raise TypeError("size must be greater than 0")
+        super().__init__(size, size)
         self.__size = size
