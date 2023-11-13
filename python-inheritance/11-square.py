d2466b7421bac37c0614843b9b25a05b1489357d
PMayala <p.mayala@alustudent.com>
2023-11-13 01:51:19 -0800

    square1

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -18,6 +18,48 @@ class Square(Rectangle):
         """Return description of the square"""
         return str("[Square] {}/{}".format(self.__size, self.__size))
 
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        self.integer_validator("size", value)
+        self.__size = value
+        self.width = value
+        self.height = value
+
     def area(self):
         """Return area of the square"""
         return self.__size * self.__size  # Corrected the area calculation
+
+if __name__ == "__main__":
+    s = Square(5)
+    print(s)
+    print(s.area())
+    print(s.width)
+    print(s.height)
+    print(s.size)
+
+    # Additional cases
+    try:
+        s = Square()
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        s = Square([12, 52])
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        s = Square(-35)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        s = Square(0)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
