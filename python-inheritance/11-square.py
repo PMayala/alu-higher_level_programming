3084f7254a0967ddfa01d8fbc31d278c1e17f277
PMayala <p.mayala@alustudent.com>
2023-11-13 02:24:21 -0800

    squa

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,46 +1,23 @@
 #!/usr/bin/python3
-"""Module 11-square"""
+"""
+more class base
+"""
 
 
-class BaseGeometry:
-    """Class BaseGeometry"""
-    def area(self):
-        """Area method (not implemented)"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate the value as an integer greater than 0"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Rectangle(BaseGeometry):
-    """Class Rectangle inherited from BaseGeometry"""
-    def __init__(self, width, height):
-        """Initializer with width and height"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Calculate and return the area of the rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Return the string representation of the rectangle"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+"""
+Square class
+"""
 
 
 class Square(Rectangle):
-    """Class Square inherited from Rectangle"""
+    """ Square Class """
     def __init__(self, size):
-        """Initializer with size"""
-        super().__init__(size, size)
-        self.integer_validator("size", size)
+        """ size init"""
+        self.__size = size
+        super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        """Return the string representation of the square"""
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
