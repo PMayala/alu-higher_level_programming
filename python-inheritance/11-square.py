4dc8ca1c5280ea382f1e8e71069df48536e79966
PMayala <p.mayala@alustudent.com>
2023-11-17 00:10:29 -0800

    gg

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,34 +1,15 @@
 #!/usr/bin/python3
-"""
-Module 11-square
-Contains the Square class
-"""
+"""Square class Module"""
+Rectangle = __import__("9-rectangle").Rectangle
 
-Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
-    """
-    A representation of a square
-    """
-    def __init__(self, size):
-        """
-        Initializes a new instance of Square
+    """Square class"""
 
-        Parameters:
-        - size (int): the size of the square
-        """
+    def __init__(self, size):
+        """Method for initializing a square"""
         super().__init__(size, size)
 
     def __str__(self):
-        """
-        Returns the square description
-
-        Returns:
-        str: [Square] <width>/<height>
-        """
+        """Method that returns a string"""
         return "[Square] {}/{}".format(self.__width, self.__height)
-
-if __name__ == "__main__":
-    s = Square(13)
-    print(s)
-    print(s.area())
