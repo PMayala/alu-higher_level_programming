ff246092368d50a16c42d923b9d53e8313bb527c
PMayala <p.mayala@alustudent.com>
2023-11-30 20:57:56 +0000

    gg

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# 4-header.sh
-
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-header="X-HolbertonSchool-User-Id: 98"
-
-curl -sH "$header" "$url"
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
