8508220359da1dc66554f832450e9e825aaeff58
PMayala <p.mayala@alustudent.com>
2023-11-30 20:04:31 +0000

    ii

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""
-A script that fetches different URLs using urllib.
+"""A script that
+fetches https://intranet.hbtn.io/status.
 """
 
-import urllib.request
-
-def fetch_url(url):
-    """
-    Fetches a URL and prints relevant information.
-    """
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(content)}")
-        print(f"\t- content: {content}")
-        print(f"\t- utf8 content: {content.decode('utf-8')}")
 
 if __name__ == '__main__':
-    # Fetching https://intranet.hbtn.io/status
-    fetch_url('https://intranet.hbtn.io/status')
+    import urllib.request
 
-    # Fetching http://0.0.0.0:5050/status instead of https://intranet.hbtn.io/status
-    fetch_url('http://0.0.0.0:5050/status')
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+        content = resp.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
