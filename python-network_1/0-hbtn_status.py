33397c3a22ae022cde16521a847bbffd2e666751
PMayala <p.mayala@alustudent.com>
2023-12-01 00:23:26 -0800

    hh

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
+"""__summary__
+- Write a Python script that fetches https://alu-intranet.hbtn.io/status
+- using the urllib package.
 """
-A script that fetches https://alu-intranet.hbtn.io/status using urllib.
-"""
-
 import urllib.request
 
+
 if __name__ == '__main__':
-    url = 'https://alu-intranet.hbtn.io/status'
-
-    with urllib.request.urlopen(url) as response:
-        content = response.read().decode('utf-8')
-
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content))
-
-# Date: 2023-12-01 00:10:37 -0800
-# README.md exists and is not empty
-# File is present
-# Correct output for fetching https://intranet.hbtn.io/status
-# Correct output for fetching http://0.0.0.0:5050/status instead of https://intranet.hbtn.io/status
-# PEP8 validation
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
