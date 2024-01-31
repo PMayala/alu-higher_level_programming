8bf8d3f842cc2f9f3e113c83ab49911737c6e155
PMayala <p.mayala@alustudent.com>
2024-01-31 08:42:11 -0800

    2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const argcount = process.argv.length;
+// Prints a message depending of the number of arguments passed
 
-if (argcount === 2){
-	console.log('No Argument');
-} else if (argcount === 3){
-	console.log('Argument found');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
