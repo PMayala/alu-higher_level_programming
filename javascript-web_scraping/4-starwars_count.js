de9d6766c452a7efa503a9c8ae865eeebb42bd1f
PMayala <p.mayala@alustudent.com>
2024-02-06 06:54:20 +0000

    4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,34 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const apiUrl = process.argv[2];
-
-request(apiUrl, function (err, response, body) {
-  if (err) {
-    console.error('Error:', err);
-    return; // Exit the function if an error occurs
-  }
-
-  try {
-    const films = JSON.parse(body).results;
-    let counter = 0;
-
-    for (let result = 0; result < films.length; result++) {
-      const characters = films[result].characters;
-
-      // Check if Wedge Antilles is present in the characters array
-      const wedgeAntilles = characters.find(character => 
-        character === 'https://swapi-api.alx-tools.com/api/people/18/' || 
-        character === 'http://swapi-api.alx-tools.com/api/people/18/'
-      );
-
-      // Increment the counter if Wedge Antilles is found
-      if (wedgeAntilles) {
-        counter++;
-      }
-    }
-
-    console.log('Count of movies where Wedge Antilles is present:', counter);
-  } catch (error) {
-    console.error('Error parsing response:', error);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
