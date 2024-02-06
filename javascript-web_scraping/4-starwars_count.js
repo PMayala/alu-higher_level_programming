b326cfc74e218ae232eff479014ed07e4f7b7b48
PMayala <p.mayala@alustudent.com>
2024-02-06 06:45:52 +0000

    web

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
+const apiUrl = process.argv[2];
 
-request(url, function (err, data, body) {
+request(apiUrl, function (err, response, body) {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
     let counter = 0;
     const films = JSON.parse(body).results;
+
     for (let result = 0; result < films.length; result++) {
       const characters = films[result].characters;
-      for (let j = 0; j < characters.length; j++) {
-        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
-          counter += 1;
-        }
+
+      const wedgeAntilles = characters.find(character => character === 'https://swapi-api.alx-tools.com/api/people/18/' || character === 'http://swapi-api.alx-tools.com/api/people/18/');
+
+      if (wedgeAntilles) {
+        counter += 1;
       }
     }
+
     console.log(counter);
   }
 });
