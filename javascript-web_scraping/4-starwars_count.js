0970efd8ca43e90728c200a4a32338350e4d914d
PMayala <p.mayala@alustudent.com>
2024-02-06 06:50:09 +0000

    4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,21 +4,31 @@ const apiUrl = process.argv[2];
 
 request(apiUrl, function (err, response, body) {
   if (err) {
-    console.error(err);
-  } else {
-    let counter = 0;
+    console.error('Error:', err);
+    return; // Exit the function if an error occurs
+  }
+
+  try {
     const films = JSON.parse(body).results;
+    let counter = 0;
 
     for (let result = 0; result < films.length; result++) {
       const characters = films[result].characters;
 
-      const wedgeAntilles = characters.find(character => character === 'https://swapi-api.alx-tools.com/api/people/18/' || character === 'http://swapi-api.alx-tools.com/api/people/18/');
+      // Check if Wedge Antilles is present in the characters array
+      const wedgeAntilles = characters.find(character => 
+        character === 'https://swapi-api.alx-tools.com/api/people/18/' || 
+        character === 'http://swapi-api.alx-tools.com/api/people/18/'
+      );
 
+      // Increment the counter if Wedge Antilles is found
       if (wedgeAntilles) {
-        counter += 1;
+        counter++;
       }
     }
 
-    console.log(counter);
+    console.log('Count of movies where Wedge Antilles is present:', counter);
+  } catch (error) {
+    console.error('Error parsing response:', error);
   }
 });
